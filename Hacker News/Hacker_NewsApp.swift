e138b4697117d92f66f2b71cd8ce83a79df5f42a
toLight <tolightbook@gmail.com>
2022-12-31 13:57:53 +0200

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Denys Nazymok on 31.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
